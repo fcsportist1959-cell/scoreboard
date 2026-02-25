14702bbce6b60767d6bbca097d42b2856bf77ce2
fcsportist1959-cell <fcsportist1959@gmail.com>
2026-02-25 14:05:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -47,4 +47,5 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server running` bits));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
